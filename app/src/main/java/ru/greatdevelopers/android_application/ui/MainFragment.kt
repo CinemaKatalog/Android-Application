60a00dbfd72d1f13b54e7326cb84867442e6d316
devBeginer <sfr0@ya.ru>
2020-07-30 14:14:21 +0400

    main app bar begining

--- app/src/main/java/ru/greatdevelopers/android_application/ui/MainFragment.kt
+++ app/src/main/java/ru/greatdevelopers/android_application/ui/MainFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import kotlinx.android.synthetic.*
 import ru.greatdevelopers.android_application.R
 
 class MainFragment : Fragment(){
+    private lateinit var collapsingToolbarLayout : CollapsingToolbarLayout
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
